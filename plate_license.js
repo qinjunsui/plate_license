55f0b210f93ce938c883d9f183cec90ff5de6817
Qinjun Sui <ext.qinjun.sui@king.com>
2021-06-06 13:48:50 -0700

    Build a class with class variable

--- plate_license.js
+++ plate_license.js
@@ -1,1 +1,12 @@
-console.log('hello world')
+class PlateLicense {
+    //构建class的时候run的method
+    constructor() {
+        this.licenses = []
+    }
+}
+
+
+//create a new class instance
+const pl = new PlateLicense();
+//use the variable of this class
+console.log(pl.licenses)
