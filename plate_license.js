c42c23cd131bc255f4c96b71a49760171263e1c2
Qinjun Sui <ext.qinjun.sui@king.com>
2021-06-06 20:45:49 -0700

    Add generateNewLiscense method with test

--- plate_license.js
+++ plate_license.js
@@ -13,6 +13,37 @@ class PlateLicense {
     getAllLicenses() {
         return this.licenses;
     }
+
+    generateNewLicense() {
+        const numbers = Array(10).fill().map((_, i) => i.toString());
+        const letterCodes = Array(26).fill().map((_, i) => i);
+        //ascii conversion => 'A', 'B' ... 'Z'
+        const letters = letterCodes.map(code => {
+            return String.fromCharCode(code + 65)  //lowercase: + 97
+        })
+        const licenseNumber =
+            this._getRandomItemFromArray(numbers) +
+            this._getRandomItemFromArray(letters) +
+            this._getRandomItemFromArray(letters) +
+            this._getRandomItemFromArray(letters) +
+            this._getRandomItemFromArray(numbers) +
+            this._getRandomItemFromArray(numbers) +
+            this._getRandomItemFromArray(numbers);
+
+        const licenseObject = {
+            license: licenseNumber,
+            registered: new Date().getTime(),
+            status: 'REGISTERED',
+        };
+        console.log(licenseObject.license)
+        this.licenses.push(licenseObject);
+        return licenseObject;
+    }
+
+    _getRandomItemFromArray(arr) {   //private function, no need test
+        const randomIndex = Math.floor(Math.random() * arr.length);
+        return arr[randomIndex];
+    }
 }
 
 
--- test/plate_license.spec.js
+++ test/plate_license.spec.js
@@ -1,7 +1,7 @@
 const PlateLicense = require("../plate_license");
 let pl = new PlateLicense();
 
-beforeEach(()=>{
+beforeEach(() => {
     pl = new PlateLicense();
 })
 
@@ -18,3 +18,45 @@ describe("#2 Track records", () => {
         expect(pl.getAllLicenses().length).toBe(1);
     });
 });
+
+describe("#3 Generate Plate Licensee", () => {
+    // const licenseObject = pl.generateNewLicense();
+    // const licenseNumber = licenseObject.license;
+    
+
+    describe("For license number", () => {
+        const { license } = pl.generateNewLicense();
+        it("it has 7 digits", () => {
+            expect(license.length).toBe(7)
+        });
+
+        it("should have 4 numbers", () => {
+            const numbers = license.split("").filter(item => {
+                return !isNaN(item)
+            }); // i.e. '6LZD123' => ['6','1','2','3']
+            expect(numbers.length).toBe(4);
+        });
+
+        it("should have 3 letters in uppercase", () => {
+            // i.e. '6LZD123'.match(/[A-Z]/g); => ['L','Z','D']
+            const letters = license.match(/[A-Z]/g);
+            expect(letters.length).toBe(3)
+        })
+    });
+
+    it("have register time defined", () => {
+        const { registered } = pl.generateNewLicense();
+        expect(registered).toBeDefined();
+    });
+
+    it("have status is REGISTERED", () => {
+        const { status } = pl.generateNewLicense();
+        expect(status).toBe("REGISTERED");
+    });
+
+    it("updates the licenses variables in class", () => {
+        pl.generateNewLicense();
+        expect(pl.getAllLicenses().length).toBe(2)
+    })
+});
+
