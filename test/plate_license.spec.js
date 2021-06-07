449a2afa4b8e00c6556d5845834e4cb8101a0c92
Qinjun Sui <ext.qinjun.sui@king.com>
2021-06-06 19:18:14 -0700

    Add testing for getAllLicenses

--- test/plate_license.spec.js
+++ test/plate_license.spec.js
@@ -1,10 +1,20 @@
 const PlateLicense = require("../plate_license");
+let pl = new PlateLicense();
 
+beforeEach(()=>{
+    pl = new PlateLicense();
+})
 
 describe("#1 Generate PlateLicense class", () => {
     it("successfully generate the instance", () => {
         const pl = new PlateLicense();
         expect(pl).toBeDefined();
     });
-    
+});
+
+describe("#2 Track records", () => {
+    it("successfully get all plate licenses", () => {
+        const pl = new PlateLicense();
+        expect(pl.getAllLicenses().length).toBe(1);
+    });
 });
